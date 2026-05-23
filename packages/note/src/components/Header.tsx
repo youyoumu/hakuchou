6711f0277f7d0def4f0c698d172306dae83913a0
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-05-23 16:47:55 +0700

    feat: add header

---
+++ packages/note/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import { useGeneralContext } from "#/contexts/GeneralContext";
+import { constant } from "#/lib/constant";
+
+export function Header() {
+  const { $general } = useGeneralContext();
+
+  return (
+    <div
+      class="top-0 left-0 w-full pt-2 pb-2 sm:pt-4 bg-base-100/90 backdrop-blur-xs z-30"
+      classList={{
+        fixed: !$general.isAnkiWeb,
+        absolute: $general.isAnkiWeb,
+      }}
+    >
+      <div class="w-full mx-auto px-2 sm:px-4 max-w-4xl">
+        <div class="flex justify-between flex-row h-6 items-center min-h-6">
+          <div class="flex gap-1 sm:gap-2 items-center">
+            <div class="relative">
+              <div class="text-base-content-soft">{constant.VERSION}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
