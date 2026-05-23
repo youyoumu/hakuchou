b7c4c8bdab4bf2facb7b0e2be4e03b8e29296d52
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-05-23 15:50:08 +0700

    feat: display 2 type

--- packages/note/src/components/Back.tsx
+++ packages/note/src/components/Back.tsx
@@ -1,12 +1,14 @@
 import { useAnkiFieldContext } from "#/contexts/AnkiFieldsContext";
-import { onMount } from "solid-js";
+import { onMount, Show } from "solid-js";
 import { unwrap } from "solid-js/store";
 import { Sentence } from "./Sentence";
 import { Definition } from "./Definition";
 import { Expression } from "./Expression";
+import { useCardContext } from "#/contexts/CardContext";
 
 export function Back() {
   const { $ankiFields } = useAnkiFieldContext<"back">();
+  const { $cardType } = useCardContext();
 
   onMount(() => {
     if (globalThis.HAKUCHOU) {
@@ -19,6 +21,12 @@ export function Back() {
       <Expression type={1} />
       <Definition type={1} />
       <Sentence type={1} />
+      <Show when={$cardType() === "taigigo-ruigigo"}>
+        <div class="divider"></div>
+        <Expression type={2} />
+        <Definition type={2} />
+        <Sentence type={2} />
+      </Show>
     </>
   );
 }
