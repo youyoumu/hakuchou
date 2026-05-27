7073b3178d807aab0f674e51b06627fd3ded6703
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-05-27 15:45:46 +0700

    fix: empty Bekkai

--- packages/note/src/components/Definition.tsx
+++ packages/note/src/components/Definition.tsx
@@ -25,7 +25,9 @@ export function Definition(props: { type: 1 | 2 }) {
   const $glossary = createMemo(() =>
     $variant() === 1 ? $ankiFields.Glossary : $ankiFields.Glossary2,
   );
-  const $bekkai = createMemo(() => $ankiFields.Bekkai.split("|").map((line) => line.trim()));
+  const $bekkai = createMemo(() =>
+    $ankiFields.Bekkai ? $ankiFields.Bekkai.split("|").map((line) => line.trim()) : [],
+  );
   const $isVerticalBekkai = createMemo(() => $bekkai().length > 3 || !bp.isAtLeast("sm"));
 
   const $pages = createMemo(() => {
